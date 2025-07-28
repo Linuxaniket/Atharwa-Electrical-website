be592275d6630233216690cc778977cb8227a8fd
Aniket Dahire <aniketdahire476@gmail.com>
2025-07-28 21:17:07 +0530

    Done with sitemap for seo

---
+++ next-sitemap.config.cjs
@@ -0,0 +1,5 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://atharvaelectricals.vercel.app/", // replace this with your actual domain
+  generateRobotsTxt: true,
+};
